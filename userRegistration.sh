3f02de045457aa82dc3f5b7674e58504a5f2b275
naincyrani06 <naincyrani06it@gmail.com>
2020-08-08 23:14:34 +0530

    Validating First Name Of User

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,18 @@
 #!/bin/bash -x
-echo "welcome to user Registration System"
+read -p "Enter First Name: " first_name
+
+first_name_pattern="^[[:upper:]]{1,}"
+
+first_name_count=${#first_name}
+
+if [ $first_name_count -ge 3 ]
+then
+	if [[ $first_name =~ $first_name_pattern ]]
+	then
+       		echo "First name is valid"
+	else
+       		echo "First name is invalid"
+	fi
+else
+	echo "Entered first name does not contain minimum 3 characters"
+fi
