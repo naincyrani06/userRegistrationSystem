cbe3fe70ff6045839233a76a8da018ed5475a141
naincyrani06 <naincyrani06it@gmail.com>
2020-08-08 23:38:00 +0530

    Validating Last Name Of User

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
-read -p "Enter First Name: " first_name
+read -p "Enter Last Name: " last_name
 
-first_name_pattern="^[[:upper:]]{1,}"
+last_name_pattern="^[[:upper:]]{1,}"
 
-first_name_count=${#first_name}
+last_name_count=${#last_name}
 
-if [ $first_name_count -ge 3 ]
+if [ $last_name_count -ge 3 ]
 then
-	if [[ $first_name =~ $first_name_pattern ]]
-	then
-       		echo "First name is valid"
-	else
-       		echo "First name is invalid"
-	fi
+        if [[ $last_name =~ $last_name_pattern ]]
+        then
+                echo "Last name is valid"
+        else
+                echo "Last name is invalid"
+        fi
 else
-	echo "Entered first name does not contain minimum 3 characters"
+        echo "Entered last name does not contain minimum 3 characters"
 fi
