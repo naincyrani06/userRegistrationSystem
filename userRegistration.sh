fd32a8e70b63824e9835fae90c08a6cc9162d327
naincyrani06 <naincyrani06it@gmail.com>
2020-08-09 00:43:52 +0530

    Validating Password From User

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 
-read -p "Enter the password: " password
 
-password_pattern="^[a-zA-Z0-9#@_.-]{8,}$"
+read -p "Enter the password having 8 Characters With Minimum 1 Upper Case Letter: " password
 
+password_count=${#password}
 
-if [[ $password =~ $password_pattern ]]
+if [ ${#password} -ge 8 && $password == *[[:upper:]]* ]
 then
-       echo "Entered Password is valid"
+	echo "Entered Password is Valid"
 else
-       echo "Entered Password is not valid"
+	echo "Entered Password is not Valid"
 fi
